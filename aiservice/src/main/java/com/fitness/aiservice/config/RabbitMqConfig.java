80975b1ceb5fb56927d1dfba27fa763a977c0f0e
Asmi <asmi.katrodiya@gmail.com>
2026-01-08 11:04:20 -0500

    added code for gemini API

--- aiservice/src/main/java/com/fitness/aiservice/config/RabbitMqConfig.java
+++ aiservice/src/main/java/com/fitness/aiservice/config/RabbitMqConfig.java
@@ -1,7 +1,9 @@
-package com.fitnessyoga.activityservice.config;
+package com.fitness.aiservice.config;
 
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,4 +1,23 @@
 package com.fitness.aiservice.service;
 
-public class ActivityMessageListner {
+import com.fitness.aiservice.model.Activity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityMessageListener {
+
+    private final ActivityAIService aiService;
+
+    @RabbitListener(queues = "activity.queue")
+    public void processActivity (Activity activity){
+        log.info("Received activity for processing: {}", activity.getId());
+        log.info("Generated Recommendation: {}",aiService.generateRecommendation(activity));
+
+    }
 }
